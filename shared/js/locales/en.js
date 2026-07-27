fd6dae466cbd68e42d4569726462cba38af746c4
Masanori SATAKE <61401908+masanori-satake@users.noreply.github.com>
2026-07-28 00:35:04 +0900

    New translations ja.js (English)
    [ci skip]

--- shared/js/locales/en.js
+++ shared/js/locales/en.js
@@ -124,6 +124,7 @@ export default {
     'page-break': 'Page Break',
     'btn-add-page-break': 'Add Page Break',
     'btn-add-category': 'カテゴリを追加',
+    'btn-add-custom-anim': 'カスタムアニメーションを追加',
     'confirm-delete-page-break': 'Do you want to delete this page break?',
     // Tooltips
     'tooltip-copy-report': 'Copy daily report to clipboard',
@@ -474,4 +475,6 @@ export default {
     'maker-warning-title': '警告',
     'maker-note-extension-only':
         '※ QL-Animation Makerはブラウザ拡張機能としてインストールされている場合のみ利用できます。プレビュー版では起動できません。',
+    'maker-error-name-empty': '名前を入力してください',
+    'maker-error-name-duplicate': '同じ名前のカスタムアニメーションが存在します。違う名前を入力してください。',
 };
