8be3f350bdb605e3caa70761be2fea926ef54a7f
Masanori SATAKE <61401908+masanori-satake@users.noreply.github.com>
2026-08-02 13:41:05 +0900

    New translations ja.js (English)
    [ci skip]

--- shared/js/locales/en.js
+++ shared/js/locales/en.js
@@ -152,6 +152,8 @@ export default {
     'maintenance-reset-all-desc': '作業履歴は維持されます。',
     'btn-reset-all': '全てリセット',
     'btn-reset': 'リセット',
+    'btn-initialize-all': '全て初期化',
+    'btn-initialize': '初期化',
     'maintenance-reset-settings': 'Reset Settings Only',
     'maintenance-reset-settings-desc': '作業履歴とカテゴリは維持されます。',
     'btn-reset-settings': 'Reset Settings Only',
