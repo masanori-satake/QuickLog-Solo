9fa39b9d354308b3592a6a709f1f6a3c3973fab8
Masanori SATAKE <61401908+masanori-satake@users.noreply.github.com>
2026-08-03 23:57:37 +0900

    New translations ja.js (Korean)
    [ci skip]

--- shared/js/locales/ko.js
+++ shared/js/locales/ko.js
@@ -193,6 +193,7 @@ export default {
     'backup-status-syncing': 'バックアップ中...',
     'backup-last-time': '最終バックアップ: {time}',
     'backup-directory': '디렉토리',
+    'backup-not-selected': '(未選択)',
     'backup-file-count': 'ファイル数: {count}日分',
     'backup-file-count-unit': '일치',
     'backup-last-time-label': '最終バックアップ時刻',
